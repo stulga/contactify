c6007f910b525a83c85bf76b60d730d9bcf44d66
stulga <53854926+stulga@users.noreply.github.com>
2019-08-09 12:10:22 +0300

    Updates

---
+++ precache-manifest.c82726087d3d295575da34bb787f8e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f8ca084c1f62f7fab2a2214ce0ee80",
+    "url": "/contactify/index.html"
+  },
+  {
+    "revision": "2a013d5da64eb9789d79",
+    "url": "/contactify/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/contactify/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "2a013d5da64eb9789d79",
+    "url": "/contactify/static/js/main.6f6c3328.chunk.js"
+  },
+  {
+    "revision": "a479beae4b42db83f4dd",
+    "url": "/contactify/static/js/runtime~main.036deb3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contactify/static/media/logo.5d5d9eef.svg"
+  }
+]);
